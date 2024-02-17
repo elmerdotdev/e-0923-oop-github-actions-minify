6920484c58b25f113f10b5ea6a43db037bb6b07b
Elmer Balbin <contact@elmer.dev>
2024-02-16 18:58:22 -0800

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+// Add function
+function add(a, b) {
+  return a + b;
+}
+
+// Subtract function
+function sub(a, b) {
+  return a - b;
+}
+
+// Multipy function
+function mul(a, b) {
+  return a * b;
+}
+
+// Divide function
+function div(a, b) {
+  return a / b;
+}
+
+document.querySelector('.container').style.backgroundColor = 'red'
+
